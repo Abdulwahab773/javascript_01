a04934de45c1ba6b21d2dfe6ed8fdc7dcf17b472
Abdulwahab773 <abdulwahabyou@gmail.com>
2024-12-18 02:04:14 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,131 @@
+// CHAPTER NO. 1 "ALERTS"
+
+// Question 1
+
+// alert("Hello World!");
+
+
+
+// Question 2
+
+// alert("Error! Please enter a valid password.");
+
+
+
+// Question 3
+
+// alert ("Welcome to JS Land...\nHappy Coding!");
+
+
+
+// Question 4
+
+// alert ("Welcome to JS Land...");
+
+// alert ("Happy Coding!");
+
+
+
+
+//Question No 5
+
+//console.log("Hello..I can run JS through my web browsers console");
+
+
+
+
+// CHAPTER NO. 2 "VARIABLES FOR STRINGS"
+
+
+//Question No 1
+
+// var username = null
+
+
+
+//Question No 2
+
+// var myName = ("Abdul Wahab");
+
+
+
+//Question No 3
+
+// var message = ("Hello World");
+
+// alert(message);
+
+
+
+//Question No 4
+
+// Step 1: Save the student's bio data in variables:
+
+// var Name = ("Abdul Wahab");
+// var age = ("20 Years Old");
+// var qualification = ("Intermediate");
+
+// Step 2: Show the data in alert boxes
+
+// alert(Name);
+// alert(age);
+// alert(qualification);
+
+
+
+//Question No 5
+
+
+// var pizza = ("PIZZA\nPIZZ\nPIZ\nPI\nP")
+// alert(pizza)
+
+
+
+
+//Question No 6
+
+// var email = ("My Email Address Is:  abdulwahabyou@gmail.com");
+// alert(email);
+
+
+
+// Question No 7
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book A smarter way to learn JavaScript");
+
+
+
+//Question No 8
+
+// document.write("<h2>Yah! I can write HTML content through Java Script<h2/>");
+
+
+
+//Question No 9
+
+
+// design = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// // alert (design);
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+
+
+
+// CHAPTER NO. 2 "VARIABLES FOR NUMBERS"
+
+
+
+//Question No 1
+
+// var age = ("I am 20 years old");
+// alert(age);
+
+
+
+
+//Question No 3
+
+// var birthYear = "2004"
+// document.write("<h2> My birth year is <h2/>" +birthYear);
